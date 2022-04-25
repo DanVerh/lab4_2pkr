4cebd349c4b88503fa5d202bb1cc99dc124ddb9f
DanVerh <danverh29@gmail.com>
2022-04-26 00:54:40 +0300

    2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include "../lab4_2pkr/Source.cpp"
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string s;
+			s = "ab(cd)";
+			string t = Erase(s);
+			string a = "ab";
+			Assert::AreEqual(a, t);
+		}
+	};
+}
